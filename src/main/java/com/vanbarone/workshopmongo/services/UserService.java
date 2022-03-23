94b6dbe436ebd6e2b7334fa60d8f28ef3a459c06
Vanessa Barone <vanbarone.milani@gmail.com>
2022-03-23 16:10:07 -0300

    Atualização de usuário com PUT

--- src/main/java/com/vanbarone/workshopmongo/services/UserService.java
+++ src/main/java/com/vanbarone/workshopmongo/services/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.IncorrectUpdateSemanticsDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.vanbarone.workshopmongo.domain.User;
@@ -36,6 +37,20 @@ public class UserService {
 		
 		repo.deleteById(id);
 	}
+	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		
+		updateData(newObj, obj);
+		
+		return repo.save(newObj);
+	}
+	
+	public void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+	
 	
 	public User fromDTO(UserDTO obj) {
 		return new User(obj.getId(),obj.getName(), obj.getEmail());		
