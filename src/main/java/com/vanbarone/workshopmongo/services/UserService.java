a2ae4ae0cac12a6cf80db0fbcdb5990a5e7f3956
Vanessa Barone <vanbarone.milani@gmail.com>
2022-03-23 15:56:46 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/vanbarone/workshopmongo/services/UserService.java
+++ src/main/java/com/vanbarone/workshopmongo/services/UserService.java
@@ -31,6 +31,12 @@ public class UserService {
 		return repo.insert(obj);
 	}
 	
+	public void delete(String id) {
+		findById(id);
+		
+		repo.deleteById(id);
+	}
+	
 	public User fromDTO(UserDTO obj) {
 		return new User(obj.getId(),obj.getName(), obj.getEmail());		
 	}
