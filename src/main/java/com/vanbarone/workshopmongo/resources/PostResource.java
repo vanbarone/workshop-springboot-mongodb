2f2b91ea514324106d7c41ffab4b58cbd8059150
Vanessa Barone <vanbarone.milani@gmail.com>
2022-03-24 16:25:38 -0300

    Consulta simples com query methods

--- src/main/java/com/vanbarone/workshopmongo/resources/PostResource.java
+++ src/main/java/com/vanbarone/workshopmongo/resources/PostResource.java
@@ -7,9 +7,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vanbarone.workshopmongo.domain.Post;
+import com.vanbarone.workshopmongo.resources.util.URL;
 import com.vanbarone.workshopmongo.services.PostService;
 
 @RestController
@@ -33,5 +35,14 @@ public class PostResource {
 		Post post = service.findById(id);
 		
 		return  ResponseEntity.ok().body(post);
+	}
+	
+	@RequestMapping(value="/titlesearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+		text = URL.decodeParam(text);
+		
+		List<Post> list = service.findByTitle(text);
+		
+		return  ResponseEntity.ok().body(list);
 	}
 }
--- src/main/java/com/vanbarone/workshopmongo/services/PostService.java
+++ src/main/java/com/vanbarone/workshopmongo/services/PostService.java
@@ -25,4 +25,8 @@ public class PostService {
 		
 		return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
+	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
 }
