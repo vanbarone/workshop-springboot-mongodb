1d8b0498f63d65ce5de6fc63366b0a04f5cfc861
Vanessa Barone <vanbarone.milani@gmail.com>
2022-03-24 15:45:07 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/vanbarone/workshopmongo/resources/UserResource.java
+++ src/main/java/com/vanbarone/workshopmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.vanbarone.workshopmongo.domain.Post;
 import com.vanbarone.workshopmongo.domain.User;
 import com.vanbarone.workshopmongo.dto.UserDTO;
 import com.vanbarone.workshopmongo.services.UserService;
@@ -71,6 +72,14 @@ public class UserResource {
 		obj = service.update(obj);
 		
 		return  ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		
+		User obj = service.findById(id);
+		
+		return  ResponseEntity.ok().body(obj.getPosts());
 	}
 	
 	
