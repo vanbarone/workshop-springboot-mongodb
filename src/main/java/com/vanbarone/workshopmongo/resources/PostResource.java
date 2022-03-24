31cec203c93029b5473698af0ad46830a8fd62a7
Vanessa Barone <vanbarone.milani@gmail.com>
2022-03-24 15:51:41 -0300

    Obtendo um post por id

---
+++ src/main/java/com/vanbarone/workshopmongo/resources/PostResource.java
@@ -0,0 +1,37 @@
+package com.vanbarone.workshopmongo.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vanbarone.workshopmongo.domain.Post;
+import com.vanbarone.workshopmongo.services.PostService;
+
+@RestController
+@RequestMapping(value="/posts")
+public class PostResource {
+
+	@Autowired
+	private PostService service;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findAll() {
+		
+		List<Post> list = service.findAll();
+		
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Post> findById(@PathVariable String id) {
+		
+		Post post = service.findById(id);
+		
+		return  ResponseEntity.ok().body(post);
+	}
+}
---
+++ src/main/java/com/vanbarone/workshopmongo/services/PostService.java
@@ -0,0 +1,28 @@
+package com.vanbarone.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vanbarone.workshopmongo.domain.Post;
+import com.vanbarone.workshopmongo.repositories.PostRepository;
+import com.vanbarone.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+
+	public List<Post> findAll(){
+		return repo.findAll();
+	}
+	
+	public Post findById(String id){
+		Optional<Post> post = repo.findById(id);
+		
+		return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
